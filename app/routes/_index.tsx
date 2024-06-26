ed03c3fe946568f796714ce1ac3c00a6e9d2006a
Yanai1005 <tatsuya871_1005@icloud.com>
2024-06-26 17:36:17 +0900

    見た目修正

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,13 +2,14 @@ import { Link } from '@remix-run/react';
 
 export const links = () => {
   return [
+    { rel: 'stylesheet', href: '/styles/normalize.css' },
     { rel: 'stylesheet', href: '/styles/index.css' }
   ];
 };
 
 export default function Index() {
   return (
-    <div>
+    <div className="page-container">
       <div className="container">
         <p className="subheading">じょぎ</p>
         <Link to="/login" className="link">
