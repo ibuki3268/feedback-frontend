580866d5bb2d32cddc245f3b79405cd29c82a258
ibuki <ryuku3268@gmail.com>
2025-08-16 14:22:07 +0900

    治

--- server.js
+++
@@ -1,6 +0,0 @@
-import { createRequestHandler } from "@remix-run/cloudflare";
-import * as build from "./build";  // ← プロジェクトルートの build を読み込む
-
-export default {
-  fetch: createRequestHandler(build, process.env.NODE_ENV),
-};
